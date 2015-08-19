2ccc9231bd1080fd4878b1006dbca1f4592d6e6b
Daniel Ramirez <daniel.ramirez@fsfb.edu.co>
2015-08-19 11:35:10 -0500

    Inclusión de controladores que faltaban

---
+++ application/controllers/Patients.php
@@ -0,0 +1,81 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Patients extends CI_Controller {
+    
+    /**
+    * Constructor para cargar headers, modelos y debug profiler.
+    **/
+    function __construct() {
+      parent::__construct();
+      //$this->output->enable_profiler(TRUE); //profiler para el seguimiento del performance                               
+      $this->load->helper('url');
+      $this->load->database();
+      //$this->load->library('ion_auth');
+      //$this->load->model('model');
+      $this->load->library('grocery_CRUD');
+
+      /*$this->output->set_header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+      $this->output->set_header('Cache-Control: no-cache, no-store, must-revalidate, max-age=0');
+      $this->output->set_header('Cache-Control: post-check=0, pre-check=0', FALSE);
+      $this->output->set_header('Pragma: no-cache');*/
+      
+      $this->output->set_header('Last-Modified: ' . gmdate("D, d M Y H:i:s") . ' GMT');('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+      $this->output->set_header('Cache-Control: post-check=0, pre-check=0', FALSE);
+      $this->output->set_header('Pragma: no-cache');
+      $this->output->set_header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
+    }
+    
+    /**
+     *  Función que pinta el menú principal de la aplicación (una vez ingreso de usuario).
+     */
+    public function view() {
+      $data['page'] = 'Pacientes';
+      
+      // Inicialización CRUD
+      $crud = new grocery_CRUD();
+      $crud->set_subject('Pacientes');
+      $crud->set_table('patients');
+      $crud->columns('type_id','id_number','first_name','middle_name','last_name','contact_phone','institution');
+
+      // Labels de columnas
+      $crud->display_as('type_id', 'Tipo de Identificación');
+      $crud->display_as('id_number', 'No. Identificación');
+      $crud->display_as('first_name', 'Primer Nombre');
+      $crud->display_as('middle_name', 'Segundo Nombre');
+      $crud->display_as('last_name', 'Primer Apellido');
+      $crud->display_as('second_last_name','Segundo Apellido');
+      $crud->display_as('birth_date','Fecha de Nacimiento');
+      $crud->display_as('gender','Género');
+      $crud->display_as('contact_phone', 'Teléfono de Contacto');
+      $crud->display_as('institution', 'Institución');
+      
+      // Campos obligatorios
+      $crud->required_fields('type_id','id_number','first_name','last_name','birth_date','gender','contact_phone','institution');
+      
+      // Validación de campos
+      $crud->set_rules('id_number','No. Identificación','integer');
+      
+      // Dropdowns (Quemados)
+      $crud->field_type('type_id','dropdown',array(
+         'TI' => 'Tarjeta de Identidad',
+         'CC' => 'Cédula de Ciudadanía',
+         'NUIP' => 'NUIP',
+         'Pasaporte' => 'Pasaporte',
+         'CE' => 'Cédula de Extranjería'
+      ));
+      $crud->field_type('gender','dropdown',array(
+         'Femenino' => 'Femenino',
+         'Masculino' => 'Masculino',
+         'Otro' => 'Otro'
+      ));
+      
+      // Pintado de formulario y creación de vista
+      $output = $crud->render();
+      
+      $this->load->view('layout/header', $data);
+      $this->load->view('layout/navigation', $data);
+      $this->load->view('patients/list', $output);
+      $this->load->view('layout/footer', $data);
+    }
+}
---
+++ application/views/login.php
@@ -0,0 +1,75 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Orientación Médica Virtual FSFB</title>
+        <link href="<?php echo base_url() ?>/includes/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?php echo base_url() ?>/includes/css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="<?php echo base_url() ?>/includes/css/basic.css" rel="stylesheet">
+    </head>
+    <body>
+        <script src="<?php echo base_url() ?>/includes/js/jquery.min.js"></script>
+        <script src="<?php echo base_url() ?>/includes/js/bootstrap.min.js"></script>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapsed1">
+                            <span class="sr-only">Ver navegación</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">Orientación Médica Virtual</a>
+                    </div>
+                    <div id="navbar-collapsed1" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li  class="active">
+                                <a href="#">Inicio</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            
+            <div class="row">
+                <div class="jumbotron" style="background-image:url(<?php echo base_url() ?>includes/img/banner1.png); no-repeat fixed; background-size:cover; background-position: right 0px top -60px;">
+                    <h1 style="color: white; font-size: xx-large; text-shadow: black 0.05em 0.05em 0.05em;">Orientación Médica Virtual
+                    <br />
+                    <small style="color: white;">Fundación Santa Fe de Bogotá</small></h1>
+                    <div class="row" style="background-color: rgba(255,255,255,0.5); padding: 10px; border-radius: 5px;">
+                        <div class="col-xs-12 col-sm-6 col-md-6">
+                        <p style="color: black; font-size: medium; text-shadow: black 0.01em 0.01em 0.01em;">El Servicio de Orientación Médica Virtual permite a las instituciones
+                    educativas solicitar orientación y asistencia ante casos dudosos o difíciles.</p>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 col-md-6" >
+			    
+			    <?php echo form_open("home/login");?>
+			    <?php 
+				if ($message) {
+				  echo "<div id='infoMessage' class='alert alert-danger alert-dismissible' role='alert'>";
+				  echo "<button type='button' class='close' data-dismiss='alert'>";
+				  echo "<span aria-hidden='true'>&times;</span>";
+				  echo "<span class='sr-only'>Close</span>";
+				  echo "</button>";
+				  echo $message;
+				  echo "</div>";
+				}
+				?>
+				<input type="text" id="identity" name="identity" class="form-control" placeholder="Correo electrónico" required autofocus><br />
+				<input type="password" id="password" name="password" class="form-control" placeholder="Contraseña" required><br />
+				<label for="remember">Recordarme: </label>
+				<?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+				<a href="auth/forgot_password">¿Ha olvidado su contraseña?</a>
+				
+				<div class="text-right"><button type="submit" class="btn btn-lg btn-primary">Ingresar</button></div>
+			    <?php echo form_close();?>
+                        </div>
+                    </div>
+                </div>
+            </div>
